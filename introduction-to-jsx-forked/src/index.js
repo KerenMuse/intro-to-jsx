87722e0260302e044d20e8fb07b499711ef7921b
Keren <67933576+KerenMuse@users.noreply.github.com>
2023-06-02 05:02:33 +0100

    Add files via upload

---
+++ introduction-to-jsx-forked/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favourite food</h1>
+    <ul>
+      <li>Egg fried rice with sweet and sour chicken</li>
+      <li>Jerk Chicken with Rice and Peas</li>
+      <li>Jollof rice with Stew Beef</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
